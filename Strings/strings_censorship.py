a35271ce9045cf51de9d40d5c59be30af53232c2
ra0ued <ra0ued@zabtech.ru>
2025-03-06 22:30:53 +0300

    Initial commit

---
+++ Strings/strings_censorship.py
@@ -0,0 +1,5 @@
+first_word = input()
+second_word = input()
+
+# ваш код
+print(sorted(first_word) == sorted(second_word))
---
+++ main.py
@@ -0,0 +1,13 @@
+recipient = input()
+player_name = input()
+player_ships_count = input()
+reward_ships_count = input()
+
+title = '[Массовая рассылка] Уважаемый игрок Мира кораблей,' + player_name + '!'
+body = 'Спасибо, что любите и играете в нашу игру.' \
+       '\nМы заметили, что у вас всего %s' % player_ships_count + ' Корабл<подставь руками>. ' \
+                                                                  "\nПоэтому, мы дарим вам подарок:%s" % reward_ships_count + 'Корабл<подставь руками>!'
+
+total = title + body + 'Ваш промокод: {<вставь промокод>}'
+
+print(total)
