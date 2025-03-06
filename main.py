a1671d7e3270635ddfece86b494a6fb563fc7d27
ra0ued <ra0ued@zabtech.ru>
2025-03-06 22:34:45 +0300

    Mailing in progress

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 recipient = input()
-player_name = input()
+player_name = recipient.split('@')[0]
 player_ships_count = input()
 reward_ships_count = input()
 
-title = '[Массовая рассылка] Уважаемый игрок Мира кораблей,' + player_name + '!'
+title = f'[Массовая рассылка] Уважаемый игрок Мира кораблей, {player_name}!'
 body = 'Спасибо, что любите и играете в нашу игру.' \
        '\nМы заметили, что у вас всего %s' % player_ships_count + ' Корабл<подставь руками>. ' \
                                                                   "\nПоэтому, мы дарим вам подарок:%s" % reward_ships_count + 'Корабл<подставь руками>!'
